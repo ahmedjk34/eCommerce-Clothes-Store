2625305a064b402729c018046dc53e05d71edb34
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2023-08-01 21:06:37 +0300

    initial commit

---
+++ Client/src/App.tsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./styles/pages/global-rules.scss";
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <div>dsadsad</div>;
+}
+
+export default App;
