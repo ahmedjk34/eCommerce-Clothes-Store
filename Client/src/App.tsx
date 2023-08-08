7ea720d1f62ac63cc09c4892928a3791caf2f95a
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2023-08-08 09:11:18 +0300

    Initial Styles & Components

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -2,10 +2,24 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./styles/pages/global-rules.scss";
+import Nav from "./components/Nav";
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import Men from "./components/Men";
+import Women from "./components/Women";
+import About from "./components/About";
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <div>dsadsad</div>;
+  return (
+    <>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/Men" element={<Men />}></Route>
+        <Route path="/Women" element={<Women />}></Route>
+        <Route path="/About" element={<About />}></Route>
+      </Routes>
+    </>
+  );
 }
 
 export default App;
---
+++ Client/src/components/Nav.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import styles from "../styles/pages/nav.module.scss";
+type Props = {};
+
+function Nav({}: Props) {
+  return <div className={styles.nav}></div>;
+}
+
+export default Nav;
