3d7ef55ea2414886296e3a58eedb8c3444d22c4f
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2023-08-11 18:04:25 +0300

    added navigation

--- Client/src/components/Nav.tsx
+++ Client/src/components/Nav.tsx
@@ -1,20 +1,25 @@
 import React from "react";
 import styles from "../styles/pages/nav.module.scss";
 import { FaShoppingCart, FaSearch } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
 
 type Props = {};
-
 function Nav({}: Props) {
+  const navigate = useNavigate();
+  function handelClick(page) {
+    navigate(`${page}`);
+  }
+
   return (
     <div className={styles.nav}>
       <h2 className={styles.motto}>Be The Style Icon</h2>
       <div className={styles.navOptions}>
         <h1>ELEGANT</h1>
         <div>
-          <h5>Home</h5>
-          <h5>Women</h5>
-          <h5>Men</h5>
-          <h5>About</h5>
+          <h5 onClick={() => handelClick("/")}>Home</h5>
+          <h5 onClick={() => handelClick("/women")}>Women</h5>
+          <h5 onClick={() => handelClick("/men")}>Men</h5>
+          <h5 onClick={() => handelClick("/about")}>About</h5>
         </div>
       </div>
       <div className={styles.iconHolder}>
