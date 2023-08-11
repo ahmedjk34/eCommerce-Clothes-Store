f4677c3e466a94062100e3e590086d8d9e36c291
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2023-08-11 16:00:27 +0300

    Done with home & it's styling

--- Client/src/components/Home.tsx
+++ Client/src/components/Home.tsx
@@ -1,9 +1,18 @@
 import React from "react";
-
+import video from "../assets/bgv.mp4";
+import styles from "../styles/pages/home.module.scss";
 type Props = {};
 
 function Home({}: Props) {
-  return <div>Home</div>;
+  return (
+    <div className={styles.home}>
+      <video muted autoPlay loop className={styles.heroVideo}>
+        <source src={video} type="video/mp4" />
+      </video>
+      <button>Women's Collection</button>
+      <button>Mens's Collection</button>
+    </div>
+  );
 }
 
 export default Home;
