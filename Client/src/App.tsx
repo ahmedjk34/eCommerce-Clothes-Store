c870ff3e66acfbe53c280244ee33253ba3d32141
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2023-08-13 21:05:52 +0300

    Added context for cart

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { ContextType, createContext, useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./styles/pages/global-rules.scss";
@@ -8,17 +8,28 @@ import Home from "./components/Home";
 import Men from "./components/Men";
 import Women from "./components/Women";
 import About from "./components/About";
+import { Item } from "./Types";
+import Cart from "./components/Cart";
+
+//@ts-ignore
+export const cartContext = createContext<ContextType>([]);
 function App() {
+  const [cart, setCart] = useState<Item[] | []>([]);
+  useEffect(() => {
+    console.log(cart);
+  }, [cart]);
   return (
-    <>
-      <Nav />
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/Men" element={<Men />}></Route>
-        <Route path="/Women" element={<Women />}></Route>
-        <Route path="/About" element={<About />}></Route>
-      </Routes>
-    </>
+    <cartContext.Provider value={{ cart, setCart }}>
+      <>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/Men" element={<Men />}></Route>
+          <Route path="/Women" element={<Women />}></Route>
+          <Route path="/About" element={<About />}></Route>
+        </Routes>
+      </>
+    </cartContext.Provider>
   );
 }
 
--- Client/src/components/ShopItem.tsx
+++ Client/src/components/ShopItem.tsx
@@ -1,19 +1,26 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Item } from "../Types";
 import styles from "../styles/pages/shop.module.scss";
+import { cartContext } from "../App";
+import { addToCart } from "./Functions/cartHandler";
 
 type Props = {
-  data: Item;
+  item: Item;
 };
 
-function ShopItem({ data }: Props) {
+function ShopItem({ item: item }: Props) {
+  const { cart, setCart } = useContext(cartContext);
+
   return (
-    <div className={styles.shopItem}>
-      <img src={data.image} alt={data.title}></img>
-      <div className={styles.priceHolder}>{data.price}$</div>
+    <div
+      className={styles.shopItem}
+      onClick={() => addToCart({ item, cart, setCart })}
+    >
+      <img src={item.image} alt={item.title}></img>
+      <div className={styles.priceHolder}>{item.price}$</div>
       <div className={styles.itemOverlay}>
-        <h2>{data.title}</h2>
-        <h5>{data.description}</h5>
+        <h2>{item.title}</h2>
+        <h5>{item.description}</h5>
       </div>
     </div>
   );
